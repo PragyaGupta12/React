17d08a30fa51d43fda966223aea33c87db53240c
Pragya Gupta <pragya.gupta012@gmail.com>
2023-05-07 22:06:21 +0530

    modified

--- blog/src/navbar.js
+++ blog/src/navbar.js
@@ -1,11 +1,14 @@
 const Navbar = () => {
+    const handleClick=()=>{
+        console.log("logged")
+    }
     return (
         <nav className="navbar">
             <h1>Techy Tech!</h1>
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/create">New Blog</a>
-                <button>Login</button>
+                <button onClick={handleClick}>Login</button>  {/*here we are not invoking the func on click because it will run the func even before we click on the button} */}
             </div>
         </nav>
     );
