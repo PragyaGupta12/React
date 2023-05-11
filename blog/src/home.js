e438c51c2bb6f2b62039dc5cfaae945995322588
Pragya Gupta <pragya.gupta012@gmail.com>
2023-05-12 00:43:09 +0530

    modified

--- blog/src/home.js
+++ blog/src/home.js
@@ -7,14 +7,14 @@ const Home = () => {
         [
         {title: 'My new website', body: 'lorem ipsum...', author: 'Alex', id: 1 },
         {title: 'Welcome party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2 },
-        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Mario', id: 3 }
+        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Mario', id: 3 },
         {title: 'React tricks', body: 'lorem ipsum...', author: 'Mario', id: 4 }
         ]
         )
     return (
         <div className="home">
            <BlogList blogs = { blogs } title="All Blogs!"/>
-           <BlogList blogs = { blogs } title="Mario's Blogs!"/>
+           {/* <BlogList blogs = { blogs.filter((blogval)=> blogval.author === 'Mario') } title="Mario's Blogs!"/> */}
         </div>
     );
 }
