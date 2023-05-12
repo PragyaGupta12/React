8a1c3c2a6ce52e9344ddd2be3825b3f053526e4b
Pragya Gupta <pragya.gupta012@gmail.com>
2023-05-12 23:28:18 +0530

    modified

--- blog/src/BlogList.js
+++ blog/src/BlogList.js
@@ -8,6 +8,7 @@ const BlogList = (props)=>{
             <div className="blog-preview" key={x.id}> 
                 <h2>{ x.title }</h2>
                 <p>Written by { x.author }</p>
+                <button><img src="/src/delete.png" alt="Delete" /></button>
             </div>//key property is used by react to keep a track of each item in the DOM as it outputs it
             //so if there is a change in the array/list, react can keep track of those items
             // and the id should be unique inside key property
