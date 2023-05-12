debc6980cc4611a2613662fee944cb3bd6d8c6ee
Pragya Gupta <pragya.gupta012@gmail.com>
2023-05-13 01:35:34 +0530

    modified

--- blog/src/BlogList.js
+++ blog/src/BlogList.js
@@ -9,7 +9,7 @@ const BlogList = (props)=>{
             <div className="blog-preview" key={x.id}> 
                 <h2>{ x.title }</h2>
                 <p>Written by { x.author }</p>
-                <button id="del-btn" onClick={() => handleDelete(x.id)}><img src="/blog/src/delete.png" alt="Delete"/></button>
+                <button id="del-btn" onClick={() => handleDelete(x.id)}><img src={"./blog/src/del.jpg"} alt="Delete"/></button>
             </div>//key property is used by react to keep a track of each item in the DOM as it outputs it
             //so if there is a change in the array/list, react can keep track of those items
             // and the id should be unique inside key property
--- blog/src/home.js
+++ blog/src/home.js
@@ -5,20 +5,21 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(
         [
-        {title: 'My new website', body: 'lorem ipsum...', author: 'Alex', id: 1 },
-        {title: 'Welcome party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2 },
-        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Mario', id: 3 },
-        {title: 'React tricks', body: 'lorem ipsum...', author: 'Mario', id: 4 }
+            { title: 'My new website', body: 'lorem ipsum...', author: 'Alex', id: 1 },
+            { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2 },
+            { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Mario', id: 3 },
+            { title: 'React tricks', body: 'lorem ipsum...', author: 'Mario', id: 4 }
         ]
-        )
-    const handleDelete = (id)=>{
-
-        }
+    )
+    const handleDelete = (id) => {
+        const newBlog = blogs.filter(x => x.id !== id);//if the id we are on is not equal to the id the blog is pointing at then this logic will send true
+        setBlogs(newBlog);
+    }
     return (
         <div className="home">
-           <BlogList blogs = { blogs } title="All Blogs!" handleDelete={handleDelete}/>
-           {/* inside  BlogList we have created the props so that we can use them in different components as well*/}
-           {/* <BlogList blogs = { blogs.filter((blogval)=> blogval.author === 'Mario') } title="Mario's Blogs!"/> */}
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+            {/* inside  BlogList we have created the props so that we can use them in different components as well*/}
+            {/* <BlogList blogs = { blogs.filter((blogval)=> blogval.author === 'Mario') } title="Mario's Blogs!"/> */}
         </div>
     );
 }
