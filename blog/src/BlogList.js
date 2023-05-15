64ceecab012dda5ff46a63d762534b5c4369ef5a
Pragya Gupta <pragya.gupta012@gmail.com>
2023-05-16 02:11:00 +0530

    added

--- blog/src/BlogList.js
+++ blog/src/BlogList.js
@@ -1,7 +1,7 @@
 const BlogList = (props) => {
     const blogs = props.blogs;
     const title = props.title;
-    const handleDelete = props.handleDelete;
+    // const handleDelete = props.handleDelete;
     return (
         <div className="blog-list">
             <h2>{title}</h2>
@@ -9,7 +9,7 @@ const BlogList = (props) => {
                 <div className="blog-preview" key={x.id}>
                     <h2>{x.title}</h2>
                     <p>Written by {x.author}</p>
-                    <button id="del-btn" onClick={() => handleDelete(x.id)}>Delete</button>
+                    {/* <button id="del-btn" onClick={() => handleDelete(x.id)}>Delete</button> */}
                 </div>//key property is used by react to keep a track of each item in the DOM as it outputs it
                 //so if there is a change in the array/list, react can keep track of those items
                 // and the id should be unique inside key property
--- blog/src/home.js
+++ blog/src/home.js
@@ -12,10 +12,10 @@ const Home = () => {
     //     ]
     // )
     const [blogs, setBlogs] = useState(null);//now we are going to fetch the data from the JSON file.
-    const handleDelete = (id) => {
-        const newBlog = blogs.filter(x => x.id !== id);//if the id we are on is not equal to the id the blog is pointing at then this logic will send true
-        setBlogs(newBlog);
-    };
+    // const handleDelete = (id) => {
+    //     const newBlog = blogs.filter(x => x.id !== id);//if the id we are on is not equal to the id the blog is pointing at then this logic will send true
+    //     setBlogs(newBlog);
+    // };
 
     useEffect(()=>{
         fetch("http://localhost:8000/blogs")//this link here has the resource i.e the blogs in the json file
@@ -28,10 +28,11 @@ const Home = () => {
 
     return (
         <div className="home">
-            {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" /* handleDelete={handleDelete} */ />}
             {/* we have written && because when the fetch() takes time to get the data the web app throws error
             so we have used a logical AND, this makes sure that the bloglist part runs only if blogs is true and 
-            that will be true only when the fetch() gets the data. This way the error is handled */}
+            that will be true only when the fetch() gets the data and then we change the state and that's how blogs
+            has the data. This way the error is handled */}
             {/* inside  BlogList we have created the props so that we can use them in different components as well*/}
             {/* <BlogList blogs = { blogs.filter((blogval)=> blogval.author === 'Mario') } title="Mario's Blogs!"/> */}
         </div>
